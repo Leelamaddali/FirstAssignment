9fac62ade3d4d90288f40b1224832c056977255d
aluguvellyakhilareddy <59576897+aluguvellyakhilareddy@users.noreply.github.com>
2020-01-16 22:22:11 -0500

    Revert "Akhila"
    This reverts commit f1dbb9742331641c5b0b3b11d638ad06467813e2.

--- Akhila/Descending Order.txt
+++
@@ -1,5 +0,0 @@
-function descendingOrder(n){
-   return parseInt(n.toString(10).split('').sort(function(a, b){
-      return b - a;
-    }).join(''));
-}
--- Akhila/Square every digit.js
+++
@@ -1,8 +0,0 @@
-function squareDigits(num){
-    var string = num.toString();
-    var results = [];
-    for (var i = 0; i < string.length; i++){
-        results[i] = string[i] * string[i];
-    }
-    return Number(results.join(''));
-};
